eebc1bef6552d67cc5ab25cc31a10ab128324850
abhaypsamrat <abhay.p.samrat@gmail.com>
2023-05-24 07:43:21 +0530

    updated

--- src/components/MainHeader.jsx
+++ src/components/MainHeader.jsx
@@ -4,30 +4,29 @@ import Image from "../images/main_header.png";
 
 const MainHeader = () => {
 	return (
-		<header className="main__header">
-			<div className="container main__header-container">
-				<div className="main__header-left">
-					<h4>#100DaysOfWorkout</h4>
-					<h1>Join The Legends of the Fitness World</h1>
-					<p>
-						Aute nisi adipisicing sunt adipisicing incididunt veniam fugiat
-						labore eiusmod nisi quis ad nulla exercitation. Aute nisi
-						adipisicing sunt adipisicing incididunt veniam fugiat labore eiusmod
-						nisi quis ad nulla exercitation.
-					</p>
-					<Link to="/plans" className="btn lg">
-						Get Started
-					</Link>
-				</div>
-				<div className="main__header-right">
-					<div className="main__header-circle"></div>
-					<div className="main__header-image">
-						<img src={Image} alt="MainHeaderImage" />
-					</div>
-				</div>
-			</div>
-		</header>
-	);
+    <header className="main__header">
+      <div className="container main__header-container">
+        <div className="main__header-left">
+          <h4>#100DaysOfWorkout</h4>
+          <h1>Welcome to Kashi Fitness Club</h1>
+          <p>
+            Welcome to our Fitness Club, where fitness meets community! Our gym
+            is dedicated to providing a positive and supportive environment for
+            individuals of all fitness levels.
+          </p>
+          <Link to="/plans" className="btn lg">
+            Get Started
+          </Link>
+        </div>
+        <div className="main__header-right">
+          <div className="main__header-circle"></div>
+          <div className="main__header-image">
+            <img src={Image} alt="MainHeaderImage" />
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default MainHeader;
