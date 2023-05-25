466b220f31bacd9a96a44a150e2b9d6af16f195f
abhaypsamrat <abhay.p.samrat@gmail.com>
2023-05-25 15:58:21 +0530

    change footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Logo from "../images/logo.png";
+// import Logo from "../images/logo.png";
 import { FaLinkedin } from "react-icons/fa";
 import { FaFacebookF } from "react-icons/fa";
 import { AiOutlineTwitter } from "react-icons/ai";
@@ -8,75 +8,74 @@ import { AiFillInstagram } from "react-icons/ai";
 
 const Footer = () => {
 	return (
-		<footer>
-			<div className="container footer__container">
-				<article>
-					<Link to="/" className="logo">
+    <footer>
+      <div className="container footer__container">
+        <article>
+          {/* <Link to="/" className="logo">
 						<img src={Logo} alt="Footer Logo" />
-					</Link>
-					<p>
-						Consectetur non nostrud enim nostrud est culpa ullamco incididunt
-						nisi. Consectetur non nostrud enim nostrud est culpa ullamco
-						incididunt nisi.
-					</p>
-					<div className="footer__socials">
-						<a
-							href="https://www.linkedin.com/"
-							target="_blank"
-							rel="noreferrer noopener"
-						>
-							<FaLinkedin />
-						</a>
-						<a
-							href="https://facebook.com/"
-							target="_blank"
-							rel="noreferrer noopener"
-						>
-							<FaFacebookF />
-						</a>
-						<a
-							href="https://twitter.com/"
-							target="_blank"
-							rel="noreferrer noopener"
-						>
-							<AiOutlineTwitter />
-						</a>
-						<a
-							href="https://instagram.com/"
-							target="_blank"
-							rel="noreferrer noopener"
-						>
-							<AiFillInstagram />
-						</a>
-					</div>
-				</article>
-				<article>
-					<h4>Permalinks</h4>
-					<Link to="/about">About</Link>
-					<Link to="/plans">Plans</Link>
-					<Link to="/trainers">Trainers</Link>
-					<Link to="/gallery">Gallery</Link>
-					<Link to="/contact">Contact</Link>
-				</article>
-				<article>
-					<h4>Insights</h4>
-					<Link to="/s">Blog</Link>
-					<Link to="/s">Case Studies</Link>
-					<Link to="/s">Events</Link>
-					<Link to="/s">Communities</Link>
-					<Link to="/constact">FAQs</Link>
-				</article>
-				<article>
-					<h4>Get In Touch</h4>
-					<Link to="/contact">Plans</Link>
-					<Link to="/s">Trainers</Link>
-				</article>
-			</div>
-			<div className="footer__copyright">
-				<small> &copy; FitnessZone </small>
-			</div>
-		</footer>
-	);
+					</Link> */}
+          <p>
+            Explore our website and discover what we have to offer. We look
+            forward to welcoming you to our fitness community
+          </p>
+          <div className="footer__socials">
+            <a
+              href="https://www.linkedin.com/"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              <FaLinkedin />
+            </a>
+            <a
+              href="https://facebook.com/"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              <FaFacebookF />
+            </a>
+            <a
+              href="https://twitter.com/"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              <AiOutlineTwitter />
+            </a>
+            <a
+              href="https://instagram.com/"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              <AiFillInstagram />
+            </a>
+          </div>
+        </article>
+        <article>
+          <h4>Permalinks</h4>
+          <Link to="/about">About</Link>
+          <Link to="/plans">Plans</Link>
+          <Link to="/trainers">Trainers</Link>
+          <Link to="/gallery">Gallery</Link>
+          <Link to="/contact">Contact</Link>
+        </article>
+        <article>
+          <h4>Insights</h4>
+          <Link to="/s">Blog</Link>
+          <Link to="/s">Case Studies</Link>
+          <Link to="/s">Events</Link>
+          <Link to="/s">Communities</Link>
+          <Link to="/constact">FAQs</Link>
+        </article>
+        <article>
+          <h4>Get In Touch</h4>
+          <Link to="/contact">Plans</Link>
+          <Link to="/s">Trainers</Link>
+        </article>
+      </div>
+      <div className="footer__copyright">
+        <small> &copy; FitnessZone </small>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
